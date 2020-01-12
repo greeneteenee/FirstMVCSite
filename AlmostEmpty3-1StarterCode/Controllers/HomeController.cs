1c2920d496954be0a72f5783141caf81a34f546e
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2020-01-11 17:51:20 -0800

    My first MVC website using external bootstrap css link and layout page

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home page";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
